268001040b05ae4778d093efa4c8a299cc39182e
Степан Водянов <2020-01161@students.ssau.ru>
2021-11-02 13:18:22 +0400

    FIX output

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import re
 import csv
 
 # Задание №1 - Программно разархивировать архив в выбранную директорию.
-directory_to_extract_to = 'C:\\Users\\D_20\\Desktop\\ВУЗ\\Лабы Прикладное\\unpacking'  # директория извлечения файлов архива
-arch_file = "C:\\Users\\D_20\\Desktop\\ВУЗ\\Лабы Прикладное\\tiff-4.2.0_lab1.zip"  # путь к архиву
-zip_file = zipfile.ZipFile(arch_file)  # Открытие zip-архива
-zip_file.extractall(directory_to_extract_to)  # Извлечение всех файлов из архива в директорию + создание директории
+directory_to_extract_to = 'C:\\Users\\D_20\\Desktop\\ВУЗ\\Лабы Прикладное\\unpacking'
+arch_file = "C:\\Users\\D_20\\Desktop\\ВУЗ\\Лабы Прикладное\\tiff-4.2.0_lab1.zip"
+zip_file = zipfile.ZipFile(arch_file)
+zip_file.extractall(directory_to_extract_to)
 
 # Задание №2 - Найти в директории все файлы формата txt, получить значения MD5 хеша для найденных файлов и
 # вывести полученные данные на экран (имя файла и хеш).
@@ -17,9 +17,9 @@ zip_file.extractall(directory_to_extract_to)  # Извлечение всех ф
 # Получаем полные пути текстовых файлов и сохраняем их в список.
 txt_files = []
 for root, dirs, files in os.walk(directory_to_extract_to):
-    for file in files:  # бежим по файлам
-        if file.endswith('.sh'):  # если окончание .txt
-            txt_files.append(str(root + '\\' + file))  # записываем в список абсолютные пути текстовых файлов
+    for file in files:
+        if file.endswith('.sh'):
+            txt_files.append(str(root + '\\' + file))
             print(file)
 
 # Получаем значения MD5 хеша для файлов из списка и выводим полученные данные на экран
@@ -39,17 +39,15 @@ for file in txt_files:
 # регулярных выражений и методов работы со строками распарсить содержимое HTML страницы и сохранить информацию из таблицы в словарь.
 target_file_data = 'https://meduza.io/feature/2020/03/05/poslednie-dannye-po-koronavirusu-vo-vsem-mire-tablitsa'
 r = requests.get(target_file_data)
-result_dct = {}  # словарь для записи содержимого таблицы
+result_dct = {}
 
 counter = 0
-# Получение списка строк таблицы
+
 lines = re.findall(r'<div class="Table-module_row__3TH83">.*?</div>.*?</div>.*?</div>.*?</div>.*?</div>', r.text)
 for line in lines:
-    # извлечение заголовков таблицы
     if counter == 0:
         counter += 1
         continue
-    # Удаление тегов
     temp = re.sub('<[^<>]*>', ';', line)
     temp = re.sub(r'\([^()]*\)', '', temp)
     temp = re.sub(r';[;;]*;', ';', temp)
@@ -59,16 +57,14 @@ for line in lines:
     temp = re.sub('\*', '', temp)
     temp = temp.replace(u'\xa0', u' ')# sub
 
-    tmp_split = temp.split(';')  # разделяем строку на подстроки для массива
-    country_name = tmp_split[0]  # получаем название страны
+    tmp_split = temp.split(';')
+    country_name = tmp_split[0]
 
-    # извлечение данных из оставшихся столбцов (статистика)
     col1_val = tmp_split[1]
     col2_val = tmp_split[2]
     col3_val = tmp_split[3]
     col4_val = tmp_split[4]
 
-    # Запись извлеченных данных в словарь
     result_dct[country_name] = [0, 0, 0, 0]
     result_dct[country_name][0] = col1_val
     result_dct[country_name][1] = col2_val
@@ -80,10 +76,10 @@ for line in lines:
 # Задание №5 - Сохранить содержимое таблицы в новый файл, где каждая новая строка таблицы
 # сохраняется с новой строки, а отдельные столбцы отделены символом ";".
 
-output = open('C:\\Users\\D_20\\Desktop\\ВУЗ\\Лабы Прикладное\\data.csv', 'w')  # открытие файла для записи
-writer = csv.writer(output, delimiter=";")  # красивое разделение в excel
-writer.writerow(['', 'ЗАБОЛЕЛИ', 'УМЕРЛИ', 'ВЫЛЕЧИЛИСЬ', 'АКТИВНЫЕ СЛУЧАИ'])  # строка для обозначения статистики
-for key, v in result_dct.items():  # записываем словарь в таблицу
+output = open('C:\\Users\\D_20\\Desktop\\ВУЗ\\Лабы Прикладное\\data.csv', 'w', newline='')
+writer = csv.writer(output, delimiter=";")
+writer.writerow(['', 'ЗАБОЛЕЛИ', 'УМЕРЛИ', 'ВЫЛЕЧИЛИСЬ', 'АКТИВНЫЕ СЛУЧАИ'])
+for key, v in result_dct.items():
     writer.writerow([key, v[0], v[1], v[2], v[3]])
 output.close()
 
